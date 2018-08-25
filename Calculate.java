12745e2b3b56f2db10d18ca8b88994f82b0dab07
vojatelsem <vojatelsem@users.noreply.github.com>
2018-08-25 06:46:46 +0300

    Add minus pow and mult operations

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,16 @@ public class Calculate {
 	
 		public static void main(String[] args) {
 				System.out.println("Calculate");
-			
+				double first = Integer.valueOf(args[0]);
+				double second = Integer.valueOf(args[1]);
+				double summ = first + second;
+				System.out.println("Sum " + summ);
+				double multi = first * second;
+				System.out.println("Mult " + multi);
+				double minus = first - second;
+				System.out.println("Minus " + minus);
+				double pow = first * first;
+				System.out.println("Pow " + pow);
+				
 		}
 }
